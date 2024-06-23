bb694f33f64f26aa1b473c4811db85e560dc31b4
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2024-06-23 14:25:39 +0300

    - updated a test

--- src/test/java/com/example/isds/demo/service/SectionTitleServiceTest.java
+++ src/test/java/com/example/isds/demo/service/SectionTitleServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.isds.demo.service;
 
+import com.example.isds.demo.exception.SectionTitleAlreadyExistsException;
 import com.example.isds.demo.model.SectionTitle;
 import com.example.isds.demo.repository.SectionTitleRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -48,5 +50,11 @@ class SectionTitleServiceTest {
         Iterable<SectionTitle> response = sectionTitleService.getAllSectionTitles();
         assert response != null;
         verify(sectionTitleRepository).findAll();
+    }
+
+    @Test
+    void shouldThrowSectionTitleAlreadyExistsExceptionWhenCreatingSectionTitle() {
+        when(sectionTitleRepository.existsByTitle("Java Comprehension")).thenReturn(true);
+        assertThrows(SectionTitleAlreadyExistsException.class, () -> sectionTitleService.createSectionTitle(mockSectionTitle));
     }
 }
